4d36d516d64c632f7db80f36162831bae963e42c
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-02-22 22:17:33 +1100

    fixed directory

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
-const express = required("express");
-const fs = required("fs");
-const database = require("./Develop/db/db.json");
-const path = require("path")
+var express = require("express");
+var fs = require("fs");
+var notes = require("./Develop/db/db.json");
+var path = require("path")
 
 var app = express();
-
 var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //to get notes then send it to db.json
-app.get("api/database", (req, res) => {
+app.get("api/notes", (req, res) => {
     res.sendFile(path.join(_dirname, "/db/db.json"))
+    console.log(res);
 })
-console.log(res);
+
 
 //add new notes
-app.post("api/database", (req, res) => {
-    
+app.post("api/notes", (req, res) => {
+    const oldNotes = fs.readFileSync("./db/db.json");
+    const newNote = req.body;
+    console.log(newNote);
+   
+    notes.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(oldNotes))
+    res.json(newNote)
 });
 
 //delete notes (BONUS)
@@ -27,12 +33,12 @@ app.post("api/database", (req, res) => {
 
 //Redirect to index.html page
 app.get("/", function (req, res) {
-    res.sendFile(path.join(_dirname,"/public/index.html"));
+    res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
 });
 
 //Redirect to notes.html page
 app.get("/", function (req, res) {
-    res.sendFile(path.join(_dirname,"/public/notes.html"));
+    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'));
 });
 
 
