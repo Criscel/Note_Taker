527c447dfe6398920bdbc85424207df2e5de2d36
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-03-03 18:44:24 +1100

    fixed app.get

--- server.js
+++ server.js
@@ -21,18 +21,12 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-//to get notes then send it to db.json
-app.get("api/notes", (req, res) => {
-    fs.readFile(__dirname, "/db/db.json", 
-    function (err, data) {
-        if (err) throw err;
-        console.log(res, data);
-        res.json(JSON.parse(data))
-    })
-});
+ //to get notes then send it to db.json
+ app.get('/api/notes', (req, res) => res.json(JSON.parse(fs.readFileSync('db/db.json'))));
+      console.log(res);
 
 //add new notes
-app.post("api/notes", (req, res) => {
+app.post("/api/notes", (req, res) => {
     fs.readFile(__dirname, '/db/db.json', function (err,notes) {
         if (err) throw err;
 
@@ -41,6 +35,7 @@ app.post("api/notes", (req, res) => {
         var id = notes[notes.length - 1].id + 1
         var newNote = { title: req.body.title, text: req.body.text, id: id }
         var activeNote = notes.concat(newNote)
+
         fs.writeFile(__dirname + "/db/db.json", JSON.stringify(activeNote), function (error, data) {
             if (error) {
               return error
@@ -48,13 +43,16 @@ app.post("api/notes", (req, res) => {
         console.log(activeNote)
         res.json(activeNote);
       })
-    })
-  })
+    });
+  });
+
 
 //delete notes (BONUS)
-// app.delete("api/database", (req, res) => {
-//}
-
+ app.delete("api/database", (req, res) => {
+   var delNote = JSON.parse(req.params.id);
+   console.log(delNote);
+});
+ 
 
 app.listen(PORT, function() {
     console.log(`Server listening on: http://localhost:${PORT}`);
